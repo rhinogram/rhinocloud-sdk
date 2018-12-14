a580b40367a2a47e34b1e975300b093e31acf274
Grady Rogers <rogersgt819@gmail.com>
2018-12-14 09:48:12 -0500

    only stringify ENV VAR if exists

--- library/helpers.js
+++ library/helpers.js
@@ -1,7 +1,7 @@
 
 module.exports.debugLog = (...args) => {
   // boolean will get stringified in process.env for serverless and docker-compose
-  if (process.env.DEBUG.toString() === 'true') {
+  if (process.env.DEBUG && process.env.DEBUG.toString() === 'true') {
     // eslint-disable-next-line
     console.log(...args);
   }
