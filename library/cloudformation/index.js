58cb875c931ff2aa65a32b01abaa44aa9fe678e2
Grady Rogers <gradytrogers@gmail.com>
2020-11-19 10:14:24 -0500

    Use explicit parameters (#27)

--- library/cloudformation/index.js
+++ library/cloudformation/index.js
@@ -57,25 +57,12 @@ function CloudFormationWrapper(credentialsOpts) {
 
   async function createChangeSet(templatePath, stackName, parameters, notificationArns, templateUrl) {
     const changeSetName = `${stackName}-${new Date().getTime()}`;
-    const currentParameters = await getStackParameters(stackName);
-    const newParameters = getCloudFormationParameters(parameters);
-    const updatedParameters = currentParameters
-      .map(({ ParameterKey, ParameterValue }) => {
-        const newParam = newParameters.find((p) => p.ParameterKey === ParameterKey);
-        if (newParam) {
-          debugLog(`Using new parameter value for ${ParameterKey}`);
-          return { ParameterKey, ParameterValue: newParam.ParameterValue };
-        }
-        debugLog(`Using existing value for ${ParameterKey}`);
-        return { ParameterKey, ParameterValue };
-      })
-      // add new parameters, not currently listed in the existing template
-      .concat(newParameters.filter(newParam => !currentParameters.find(currParam => currParam.ParameterKey === newParam.ParameterKey)));
+    const cloudformationParams = getCloudFormationParameters(parameters);
 
     const { Id: changeSetArn } = await cf.createChangeSet({
       ChangeSetName: changeSetName,
       Capabilities: ['CAPABILITY_NAMED_IAM', 'CAPABILITY_IAM'],
-      Parameters: updatedParameters,
+      Parameters: cloudformationParams,
       StackName: stackName,
       ...templatePath && { TemplateBody: fs.readFileSync(templatePath, 'utf-8') },
       ...templateUrl && { TemplateURL: templateUrl },
